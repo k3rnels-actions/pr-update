32aa3e4522cbdac573e55f56edaaae4c7f93a1bf
k3rnelpan1c-dev <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2022-01-02 19:11:17 +0100

    fix incorrect eslint error by suppressing it

--- src/model/types.ts
+++ src/model/types.ts
@@ -1,4 +1,6 @@
 import * as github from '@actions/github'
+// suppress eslint since its stupid and fails to lookup components that is 100% exported and can be imported here
+// eslint-disable-next-line import/named
 import {components as OctoOpenApiTypes} from '@octokit/openapi-types'
 
 export type Octokit = ReturnType<typeof github.getOctokit>
