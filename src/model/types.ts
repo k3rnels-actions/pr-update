a5b2cdffee063a8ce6da8e36514e387d66e4c492
k3rnelpan1c-dev <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2022-06-19 19:57:49 +0200

    remove: unused branch type definition

--- src/model/types.ts
+++ src/model/types.ts
@@ -6,5 +6,4 @@ import {components as OctoOpenApiTypes} from '@octokit/openapi-types'
 export type Octokit = ReturnType<typeof github.getOctokit>
 
 export type Repo = OctoOpenApiTypes['schemas']['repository']
-export type Branch = OctoOpenApiTypes['schemas']['branch-short']
 export type PullRequest = OctoOpenApiTypes['schemas']['pull-request']
