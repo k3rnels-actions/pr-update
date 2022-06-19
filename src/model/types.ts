a74f3ae434f05b462b3277f9168de07c5008a2c9
k3rnelpan1c-dev <69395733+k3rnelpan1c-dev@users.noreply.github.com>
2022-06-19 20:06:03 +0200

    fix: suppress incorrect reported eslint violation

--- src/model/types.ts
+++ src/model/types.ts
@@ -1,6 +1,7 @@
 import * as github from '@actions/github'
-// suppress eslint since its stupid and fails to lookup components that is 100% exported and can be imported here
-// eslint-disable-next-line import/named
+/* eslint-disable-next-line --
+ * suppress eslint since its stupid and fails to lookup components that are 100% exported and can be imported here
+ */
 import {components as OctoOpenApiTypes} from '@octokit/openapi-types'
 
 export type Octokit = ReturnType<typeof github.getOctokit>
